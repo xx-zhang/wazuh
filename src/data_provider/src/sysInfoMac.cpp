d4cad278f3a2aaed3e83205d9a2cc57ee5a10e78
MiguelazoDS Cazajous-Miguel <miguel.cazajous@wazuh.com>
2023-01-25 11:49:41 -0300

    Added extra paths to search for PKG packages NOT in .app directories

--- src/data_provider/src/sysInfoMac.cpp
+++ src/data_provider/src/sysInfoMac.cpp
@@ -50,7 +50,9 @@ static const std::map<std::string, int> s_mapPackagesDirectories =
     { "/System/Applications", PKG},
     { "/System/Applications/Utilities", PKG},
     { "/System/Library/CoreServices", PKG},
-    { "/usr/local/Cellar", BREW},
+    { "/private/var/db/receipts", PKG},
+    { "/Library/Apple/System/Library/Receipts", PKG},
+    { "/usr/local/Cellar", BREW}
 };
 
 static nlohmann::json getProcessInfo(const ProcessTaskInfo& taskInfo, const pid_t pid)
@@ -105,7 +107,7 @@ static void getPackagesFromPath(const std::string& pkgDirectory, const int pkgTy
     {
         if (PKG == pkgType)
         {
-            if (Utils::endsWith(package, ".app"))
+            if (Utils::endsWith(package, ".app") || Utils::endsWith(package, ".plist"))
             {
                 nlohmann::json jsPackage;
                 FactoryPackageFamilyCreator<OSPlatformType::BSDBASED>::create(std::make_pair(PackageContext{pkgDirectory, package, ""}, pkgType))->buildPackageData(jsPackage);
